5392a9da0629cc0827e419a7b829640bde0ca51c
Ethan <16487380+mdenge@users.noreply.github.com>
2025-09-24 08:25:36 +0800

    update rules

--- surge/script/amdc.js
+++ surge/script/amdc.js
@@ -1,1 +1,4 @@
-let url=$request.url,body=null;if(url.includes("amdc/mobileDispatch")){let e=$request.headers,t=e["User-Agent"]||e["user-agent"];t.includes("AMap")||t.includes("Cainiao")?"undefined"!=typeof $task?$done({status:"HTTP/1.1 404 Not Found"}):$done():$done({})}else console.log("匹配到其他url:\n"+url);body?$done({body}):$done({});
+
+const version = 'V1.0.11';
+
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"];/(AMap|Cainiao|%E9%97%B2%E9%B1%BC|%E9%A3%9E%E7%8C%AA%E6%97%85%E8%A1%8C|%E5%96%B5%E8%A1%97|%E5%A4%A9%E7%8C%AB|Alibaba|Hema4iPhone|Moon|DMPortal)/.test(ua)?$done({body:"ddgksf2013"}):$done({});
