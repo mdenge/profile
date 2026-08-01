d5b5e817ee2817834832ac5bde69044b04933fd1
Ethan <16487380+mdenge@users.noreply.github.com>
2026-08-01 16:10:48 +0800

    surge rule update.

--- surge/script/dianping.js
+++
@@ -1,1 +0,0 @@
-const url=$request.url,header=$request.headers,resp={},headopt=header["M-SHARK-TRACEID"]||header["m-shark-traceid"];null!=headopt?$done({body:"",headers:"",status:"HTTP/1.1 204 No Content"}):$done({});
